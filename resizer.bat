5d43514c7c03ae46ed1404cad53319b821313299
Skytro <skytro@skykopf.de>
2025-05-23 04:43:58 +0200

    Add files via upload

---
+++ resizer.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+:: Get the full path of the dragged file
+set "filepath=%~1"
+set "filename=%~n1"
+set "ext=%~x1"
+
+:: Create folder
+mkdir "%filename%" 2>nul
+
+:: Resize images
+magick "%filepath%" -resize 18x18 "%filename%\%filename%-18x18.png"
+magick "%filepath%" -resize 36x36 "%filename%\%filename%-36x36.png"
+magick "%filepath%" -resize 72x72 "%filename%\%filename%-72x72.png"
+
+echo Done resizing %filename%
+pause
